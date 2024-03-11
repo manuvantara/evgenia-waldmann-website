91c7e58d48e12428cdb1f2c2e39251d488c1c921
dreamqip <v.chuchupak@gmail.com>
2024-03-11 20:36:46 +0100

    feat: add custom focus on form elements

---
+++ postcss.config.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+  plugins: [require("css-has-pseudo")],
+};
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="astro/client" />
 
+declare module "css-has-pseudo/browser";
+
 declare interface CustomElement extends HTMLElement {
   connectedCallback(): void;
   disconnectedCallback(): void;
