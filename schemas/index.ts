cf84a67ad8de8d439bf476bc6b8593c3ac471f0a
dreamqip <v.chuchupak@gmail.com>
2024-03-13 19:29:58 +0100

    feat: add sanity schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,43 @@
-export const schemaTypes = [];
+import type { SchemaTypeDefinition } from "sanity";
+
+export const schemaTypes: SchemaTypeDefinition[] = [
+  // Certificate schema
+  {
+    name: "certificate",
+    title: "Certificate",
+    type: "document",
+    fields: [
+      {
+        name: "title",
+        title: "Title",
+        type: "string",
+      },
+      {
+        name: "image",
+        title: "Image",
+        type: "image",
+      },
+    ],
+  },
+  // Review schema (youtube video url, title)
+  {
+    name: "review",
+    title: "Review",
+    type: "document",
+    fields: [
+      {
+        name: "title",
+        title: "Title",
+        type: "string",
+        description: "Optional title for the video. Displayed on hover.",
+      },
+      {
+        name: "url",
+        title: "Video URL",
+        type: "url",
+        validation: (Rule) => Rule.required(),
+        description: "Note: Does not support timestamp.",
+      },
+    ],
+  },
+];
