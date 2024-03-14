5964361cbcafe74b137a15c4995aa5cda974ca89
dreamqip <v.chuchupak@gmail.com>
2024-03-14 18:15:14 +0100

    feat: fetch certificates from CMS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -63,8 +63,6 @@ module.exports = {
       },
       animation: {
         marquee: "marquee 30s linear infinite",
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
       keyframes: {
         marquee: {
